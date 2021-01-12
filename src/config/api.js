5bd589413ee17a19986a8d8f4c16f5f56949a98c
drumovski <jason.stacy@gmail.com>
2021-01-12 22:35:12 +1100

    test for baseurl

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 //! creating a proxy request to change the base url using AXIOS
 export default axios.create({
-  baseURL: process.env.BASEURL || "http://localhost:3001",
+  // baseURL: process.env.BASEURL || "http://localhost:3001",
+  baseURL: "https://sotrpc-server.herokuapp.com",
   timeout: 5000,
   withCredentials: true,
 });
