291c04847b5b1a62c150ccaa753b3690d26e3289
drumovski <jason.stacy@gmail.com>
2021-01-12 16:08:09 +1100

    add axios call env variable

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+require('dotenv').config();
 
 //! creating a proxy request to change the base url using AXIOS
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.BASEURL || "http://localhost:3001",
   timeout: 5000,
   withCredentials: true,
 });
