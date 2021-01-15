41f961544c30337e186faf796080529493b2717b
drumovski <jason.stacy@gmail.com>
2021-01-15 11:20:36 +1100

    fix process.env for BASEURL in config

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 
 //! creating a proxy request to change the base url using AXIOS
 export default axios.create({
-  baseURL: process.env.BASEURL || "http://localhost:3002",
-  // baseURL: "https://sotrpc-server.herokuapp.com",
+  baseURL: process.env.REACT_APP_BASEURL,
   timeout: 5000,
   withCredentials: true,
 });
